672a00b14a7d152020611c6bed643db3d519c4c9
Grace Teng <grace@pelicularities.net>
2020-09-10 08:46:01 +0800

    moved new review form onto restaurant page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :find_restaurant, only: [:new, :create]
-  def new
-    @review = Review.new
-  end
+  # def new
+  #   @review = Review.new
+  # end
 
   def create
     @review = Review.new(review_params)
